0c64fa904da0e14719df555ac229e080ff444356
eyadaq <eyadaq14@gmail.com>
2025-08-10 20:40:51 +0300

    feat : the grid can be drawn now

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let 	numOfSquares= 16;
+
+let 	numOfSquares= 55;
 
 function 	drawSquares()
 {
-	let squareSize = 650/numOfSquares;
+	let squareSize = 600/numOfSquares;
 	let grid = document.querySelector(".grid");
 
 	for (let i = 0; i < numOfSquares; i++)
 	{
 		for (let x = 0; x < numOfSquares; x++)
 		{
-			div = document.createElement("div");
+			let div = document.createElement("div");
+			div.classList.add("square");
+			div.style.cssText = `width: ${squareSize}px; height: ${squareSize}px; box-sizing: border-box;`;
+			grid.append(div);
 		}
 	}
 }
