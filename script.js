1606b94840cf5252aa947b577def283e698acab7
eyadaq <eyadaq14@gmail.com>
2025-08-10 20:24:45 +0300

    Refactor HTML structure and enhance CSS styles for Etch-a-Sketch project; add JavaScript functionality for drawing squares.

---
+++ script.js
@@ -0,0 +1,15 @@
+let 	numOfSquares= 16;
+
+function 	drawSquares()
+{
+	let squareSize = 650/numOfSquares;
+	let grid = document.querySelector(".grid");
+
+	for (let i = 0; i < numOfSquares; i++)
+	{
+		for (let x = 0; x < numOfSquares; x++)
+		{
+			div = document.createElement("div");
+		}
+	}
+}
