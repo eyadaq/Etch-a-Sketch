af9eb1a8ca7f3dbc7f31eda174e992158752362a
eyadaq <eyadaq14@gmail.com>
2025-08-10 21:00:29 +0300

    Fix button onclick handler to call the correct function and improve grid drawing logic

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+function removeAllSquares(){
+	let parent = document.querySelector(".grid");
+	while (parent.firstChild)
+		parent.removeChild(parent.firstChild);
+}
 
-let 	numOfSquares= 55;
-
-function 	drawSquares()
+function 	drawSquares(numOfSquares)
 {
 	let squareSize = 600/numOfSquares;
 	let grid = document.querySelector(".grid");
-
+	removeAllSquares();
 	for (let i = 0; i < numOfSquares; i++)
 	{
 		for (let x = 0; x < numOfSquares; x++)
@@ -16,4 +19,37 @@ function 	drawSquares()
 			grid.append(div);
 		}
 	}
+}
+
+function changeGridSize()
+{
+	let input = prompt("Please enter the number of squares Max 64 Min 16:");
+	if (input === null) { 
+		return;
+	  }
+	let numOfSquares = parseInt(input);
+	if (numOfSquares === NaN)
+	{
+		alert("Error the input should be a number");
+		reset();
+		return;
+	}
+	else if (numOfSquares > 64 || numOfSquares < 16)
+	{
+		alert("Error the input should be a number between 16 and 64");
+		reset();
+		return;
+	}
+	drawSquares(numOfSquares);
+}
+
+function reset()
+{
+	removeAllSquares();
+	drawSquares(16);
+}
+
+document.addEventListener("DOMContentLoaded" ,function(){}) 
+{
+	drawSquares(16);
 }
